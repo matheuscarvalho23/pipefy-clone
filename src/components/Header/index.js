cd823d559e026907d2db6042be257aa9a244e48e
Matheus <matheus.carvalho@m2n.com.br>
2020-07-27 00:50:45 -0300

    Cloning pipefy

---
+++ src/components/Header/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+import { Container } from './style';
+
+export default function Header() {
+  return (
+    <Container>
+      <img src="https://pipestyle.staticpipefy.com/default/images/logo-white.svg" alt="Logo"/>
+    </Container>
+  )
+}
---
+++ src/components/Header/style.js
@@ -0,0 +1,19 @@
+import styled from 'styled-components';
+
+export const Container = styled.div`
+  height: 80px;
+  padding: 0 30px;
+  background: #3b5bfd;
+  color: #FFF;
+  display: flex;
+  align-items: center;
+
+  img {
+    width: 90px;
+    height: 30px;
+  }
+
+  /* @media (max-width: 600px) {
+    width: 100%;
+  } */
+`;
